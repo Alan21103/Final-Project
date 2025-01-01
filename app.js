882eb343fb7d2e56dadcf44e1b7f018e837b26ce
Alan21103 <syahlansaputra03@gmail.com>
2025-01-01 17:19:39 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const todoRoutes = require('./routes/tododb.js');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT;
+
+// Pertemuan 7
+app.use(express.static(path.join(__dirname, 'public')));
+
+const session = require('express-session');
+const authRoutes = require('./routes/authRoutes');
+const { isAuthenticated } = require('./middlewares/middleware.js');
+
+
+
+const expressLayout = require('express-ejs-layouts');
+
+const db = require('./database/db');
+
+app.use(expressLayout);
+
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
+
+app.use(express.urlencoded({ extended: true }));
+
+// Konfigurasi express-session
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false} // Set ke true jika menggunakan HTTPS
+}));
+
+app.use('/', authRoutes);
+
+app.set('view engine', 'ejs');
+
+app.get('/', isAuthenticated, (req, res) => {
+    res.render('index', {
+        layout: 'layouts/main-layouts.ejs'
+    });
+});
+
+app.get('/contact', isAuthenticated, (req, res) => {
+    res.render('contact', {
+        layout: 'layouts/main-layouts.ejs'
+    });
+});
+
+app.get('/todo-view', isAuthenticated,  (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layouts.ejs',
+            todos: todos
+        });
+    });
+});
+
+app.use('/uploads', express.static('uploads'));
+
+app.use((req, res) => {
+    res.status(404).send('404 - Page Not Found');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
